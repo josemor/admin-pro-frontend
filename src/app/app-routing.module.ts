c3106905b684be1555912f3ce8a78941a708ee4f
jlmorenoo <josel.moreno87@gmail.com>
2020-11-07 21:13:49 -0500

    Fin seccion 4 | Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
 
 const routes: Routes = [
 
-  {
-    path: '',
-    component: PagesComponent,
-    // rutas hijas: son las que pueden manejar el mismo diseño de la pagina.
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    ]
-  },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-
+  // path: '/dashboard' PagesRouting
+  // path: '/auth' AuthRouting
+  // path: '/medicos' MedicosRouting
+  // path: '/compras' ComprasRouting
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', component: NopagefoundComponent },
 ];
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {useHash: true}),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [RouterModule]
 })
---
+++ src/app/pages/pages.routing.ts
@@ -0,0 +1,41 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+
+
+
+const routes: Routes = [
+/* Se modifica el squema de rutas para que quede en un path especifico
+    {
+        path: '',
+        component: PagesComponent,
+        // rutas hijas: son las que pueden manejar el mismo diseño de la pagina.
+        children: [
+          { path: 'dashboard', component: DashboardComponent },
+          { path: 'progress', component: ProgressComponent},
+          { path: 'grafica1', component: Grafica1Component },
+          { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+        ]
+      }*/
+
+      {
+        path: 'dashboard',
+        component: PagesComponent,
+        // rutas hijas: son las que pueden manejar el mismo diseño de la pagina.
+        children: [
+          { path: '', component: DashboardComponent },
+          { path: 'progress', component: ProgressComponent},
+          { path: 'grafica1', component: Grafica1Component }
+        ]
+      }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class PagesRoutingModule {}
+
